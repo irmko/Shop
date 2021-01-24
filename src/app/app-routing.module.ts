b6b43a3a50bd4cdc63c6f3213b32bd4d9a5ee3a2
irmko <63469111+irmko@users.noreply.github.com>
2021-01-24 18:30:27 +0300

    Урок 2-4 Роутинг для меню администратора ( не работает маршрутизация )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { ProductPageComponent } from './product-page/product-page.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MainLayoutComponent} from './shared/main-layout/main-layout.component';
+import {CartPageComponent} from './cart-page/cart-page.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {ProductPageComponent} from './product-page/product-page.component';
 
 const routes: Routes = [
   {
-    path: '', component: MainLayoutComponent, children:[
+    path: '', component: MainLayoutComponent, children: [
       {path: '', redirectTo: '/', pathMatch: 'full'},
       {path: '', component: MainPageComponent},
       {path: 'product/:id', component: ProductPageComponent},
       {path: 'cart', component: CartPageComponent}
-      ]
+    ]
+  },
+  {
+    path: 'admin', loadChildren: './admin/admin.module#AdminModule'
   }
 ];
 
@@ -20,4 +23,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
